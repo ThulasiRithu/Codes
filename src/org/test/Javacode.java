79d281046f6078054b8abef5ed39424e817a5fcb
ThulasiRithu <tulsilakshmi@gmail.com>
2019-07-10 20:30:57 +0530

    Java simple codes

---
+++ src/org/test/Javacode.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Javacode {
+public static void main(String[] args) {
+	int a=12;
+	if (a%2==0) {
+		System.out.println("Even Number");
+	}
+	else {
+		System.out.println("Odd Number");
+	}
+}
+}
